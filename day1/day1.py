bdfbae684c013535b56f5f54534e9caee2b21154
Ryan Winstead <ryanwinstead@Ryans-MacBook-Air.local>
2024-12-04 18:09:00 -0800

    completed day 1 part1+2

--- day1/day1.py
+++ day1/day1.py
@@ -4,7 +4,6 @@ import numpy as np
 data = np.loadtxt("day1/input.txt")
 
 
-
 #algorithm from chatgpt
 def quick_sort(arr):
     if len(arr) <= 1:  # Base case: arrays with 0 or 1 element are already sorted
@@ -35,3 +34,40 @@ print(grand)
 
 #Answer:   = 1320851 
 
+#---------Part 2-----------------
+
+#reusing sorted lists from above 
+#col0 = quick_sort(arr0)
+#col1 = quick_sort(arr1)
+
+def count_values(array, key):
+    count_dict = {}  # Array to keep count
+    for k in key:    
+        for value in array:
+            if value == k:
+                # If the key is already in the dictionary, increment its count
+                if k in count_dict:
+                    count_dict[k] += 1
+                else:
+                    # Add the key with an initial count of 1
+                    count_dict[k] = 1
+            else:
+                # If the key doesn't appear in the array col1, assign 0
+                count_dict.get(k, 0)
+        
+    
+    return count_dict
+
+ 
+def sim_score(count_dict):  #to get similarity score
+    sum = 0
+    for key, value in count_dict.items():
+        sum = sum + (key * value)
+    return print(sum)
+
+
+
+count = count_values(col1, col0)
+sim_score(count)
+
+#Answer:    = 26859182 
